e73ac1385885b26a39f0e6b17f93090a5bd461c6
MSK1116 <anishkushwaha1116@gmail.com>
2024-06-16 22:42:17 +0545

    push

--- frontend/src/Home/Home.jsx
+++ frontend/src/Home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../component/Navbar";
 import Home_banner from "./Home_banner";
 import About from "../About/About";
 import Contact from "../contact/Contact";
+import Footer from "../component/Footer";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Home_banner />
       <About />
       <Contact />
+      <Footer />
     </>
   );
 };
