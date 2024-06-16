3b2d16d1b8b74fc6308af96813f7cc98201c8358
MSK1116 <anishkushwaha1116@gmail.com>
2024-06-16 22:17:34 +0545

    push

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = ({ sectionRef }) => {
       <div className={`max-w-screen-2xl rounded-sm container px-4 md:px-20 h-16 shadow-lg fixed top-0 left-0 right-0 z-50 ${sticky ? " bg-slate-200" : ""} `}>
         <div className=" flex justify-between items-center h-16 ">
           <div className="flex space-x-2">
-            <img src={img1 ? img1 : img2} className=" w-12 h-12 rounded-full object-cover"></img>
+            <img src={img2} className=" w-12 h-12 rounded-full object-cover"></img>
             <h1>
               Manis<span className=" text-red-700">h</span>
               <p className="text-sm">Web Portfolio</p>
