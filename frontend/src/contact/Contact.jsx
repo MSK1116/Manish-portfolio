3cdca96ba062ea854724218a855f9f24fdd3f6b8
MSK1116 <anishkushwaha1116@gmail.com>
2024-07-12 09:31:46 +0545

    push

--- frontend/src/contact/Contact.jsx
+++ frontend/src/contact/Contact.jsx
@@ -60,12 +60,12 @@ const Contact = () => {
                       <p
                         className="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
                   absolute">
-                        Password
+                        Phone NUmber
                       </p>
                       <input
                         name="phone_number"
                         placeholder="+977 9812055XX"
-                        type="password"
+                        type="text"
                         className="border placeholder-gray-400 focus:outline-none
                   focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
                   border-gray-300 rounded-md"
