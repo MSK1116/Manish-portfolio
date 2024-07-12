0d18d2a994fe6cd381ec9b80435a67480800c790
MSK1116 <anishkushwaha1116@gmail.com>
2024-07-12 09:44:46 +0545

    push

--- frontend/src/contact/Contact.jsx
+++ frontend/src/contact/Contact.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from "react";
-import Svg_team from "../SVG/Svg_team";
 
-import { SiHelpscout } from "react-icons/si";
 import Aos from "aos";
 import Svg_study_time from "../SVG/Svg_study_time";
 
@@ -60,7 +58,7 @@ const Contact = () => {
                       <p
                         className="bg-white pt-0 pr-2 pb-0 pl-2 -mt-3 mr-0 mb-0 ml-2 font-medium text-gray-600
                   absolute">
-                        Phone NUmber
+                        Phone Number
                       </p>
                       <input
                         name="phone_number"
