351655d909d7962a00ab32e7f75dc308ccba15d6
MSK1116 <anishkushwaha1116@gmail.com>
2024-07-27 17:22:02 +0545

    Done@

--- frontend/src/component/Linker.jsx
+++ frontend/src/component/Linker.jsx
@@ -1,12 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Linker = () => {
   const navigate = useNavigate();
-  useEffect(() => (location.href = "https://drive.google.com/drive/folders/1B1rD9Oeyk4gvAnBOYgjfuUDVpHpV-QLt?usp=sharing"));
+  const [message, setMessage] = useState(null);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setMessage("Failed to redirect...");
+    }, 3000);
+    const timer2 = setTimeout(() => {
+      navigate("/");
+    }, 10000);
+    return () => {
+      clearTimeout(timer);
+      clearTimeout(timer2);
+    };
+  }, [navigate]);
+
   return (
     <>
-      <h1>Redirecting...</h1>
+      {!message ? <h2>Redirecting...</h2> : <h1 className="text-red-600">Failed.. Acess to this link has been blocked by devloper.</h1>}
+
+      <br></br>
+      {!message ? "" : <h2>Returning to home in 10s</h2>}
     </>
   );
 };
