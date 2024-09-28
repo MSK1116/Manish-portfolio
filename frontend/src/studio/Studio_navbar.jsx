fd62426884f83a7a399056b080c23f7c2e48cc6d
MANU0519 <anishkushwaga1116@gmail.com>
2024-09-28 17:36:31 +0545

    push

--- frontend/src/studio/Studio_navbar.jsx
+++ frontend/src/studio/Studio_navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import img2 from "../../public/JPG_manish_circle.png";
+import img2 from "/JPG_manish_circle.png";
 import { MdOutlineMenu } from "react-icons/md";
 import { AiOutlineMenu } from "react-icons/ai";
 import { IoCloseSharp } from "react-icons/io5";
@@ -47,7 +47,7 @@ const Studio_navbar = ({ sectionRef }) => {
 
   return (
     <>
-      <div className={`max-w-screen-2xl w-screen rounded-sm container px-4 md:px-20 h-16 shadow-lg fixed top-0 left-0 right-0 z-50 ${sticky ? " bg-slate-200" : ""} `}>
+      <div className={`max-w-screen-2xl w-screen rounded-sm container px-4 md:px-20 h-16 shadow-lg bg-gray-100/75 fixed top-0 left-0 right-0 z-50 ${sticky ? " bg-slate-200" : ""} `}>
         <div className=" flex justify-between items-center h-16 ">
           <div className="flex space-x-2">
             <img src={img2} className=" w-12 h-12 rounded-full object-cover"></img>
